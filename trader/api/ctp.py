8211ab891d919de696ba816d16e0b709e4e45325
timercrack <timercrack@gmail.com>
2016-08-01 20:39:12 +0800

    add market handler

--- trader/api/ctp.py
+++ trader/api/ctp.py
@@ -16,11 +16,12 @@
 import asyncio
 import random
 
-import redis
 import ujson as json
 import aioredis
 
-import trader.utils.logger as my_logger
+from trader.utils import logger as my_logger
+from trader.api import BaseModule
+from trader.utils.func_container import param_function
 from trader.utils.read_config import *
 from trader.utils import msg_reader
 
@@ -28,59 +29,21 @@ logger = my_logger.get_logger('CTPApi')
 HANDLER_TIME_OUT = config.getint('TRADE', 'command_timeout', fallback=10)
 
 
-class CTPApi:
-    def __init__(self, io_loop: asyncio.AbstractEventLoop = None):
-        self.io_loop = io_loop
-        if self.io_loop is None:
-            self.io_loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(self.io_loop)
-        self.redis_client = redis.StrictRedis(
-            host=config.get('REDIS', 'host', fallback='localhost'),
-            db=config.getint('REDIS', 'db', fallback=1), decode_responses=True)
-        self.market_response_format = config.get('MSG_CHANNEL', 'market_response_format')
-        self.trade_response_format = config.get('MSG_CHANNEL', 'trade_response_format')
-        self.request_format = config.get('MSG_CHANNEL', 'request_format')
-        self.__request_id = random.randint(0, 65535)
+class CTPApi(BaseModule):
+    market_response_format = config.get('MSG_CHANNEL', 'market_response_format')
+    trade_response_format = config.get('MSG_CHANNEL', 'trade_response_format')
+    request_format = config.get('MSG_CHANNEL', 'request_format')
+    __request_id = random.randint(0, 65535)
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
 
     def next_id(self):
         self.__request_id = 1 if self.__request_id == 65535 else self.__request_id + 1
         return self.__request_id
-
-    async def MarketReqUserLogin(self, broker_id: str, user_id: str, password: str):
-        sub_client = None
-        channel_name1, channel_name2 = None, None
-        try:
-            sub_client = await aioredis.create_redis(
-                (config.get('REDIS', 'host', fallback='localhost'),
-                 config.getint('REDIS', 'port', fallback=6379)),
-                db=config.getint('REDIS', 'db', fallback=1))
-            request_id = self.next_id()
-            channel_name1 = self.market_response_format.format('OnRspUserLogin', '*')
-            channel_name2 = self.market_response_format.format('OnRspError', request_id)
-            ch1, ch2 = await sub_client.psubscribe(channel_name1, channel_name2)
-            cb = self.io_loop.create_future()
-            tasks = [
-                asyncio.ensure_future(msg_reader(ch1, cb), loop=self.io_loop),
-                asyncio.ensure_future(msg_reader(ch2, cb), loop=self.io_loop),
-            ]
-            param_dict = json.dumps({
-                'BrokerID': broker_id,
-                'UserID': user_id,
-                'Password': password,
-                'RequestID': request_id,
-            })
-            self.redis_client.publish(self.request_format.format('MarketReqUserLogin'), param_dict)
-            rst = await asyncio.wait_for(cb, HANDLER_TIME_OUT, loop=self.io_loop)
-            await sub_client.punsubscribe(channel_name1, channel_name2)
-            sub_client.close()
-            await asyncio.wait(tasks, loop=self.io_loop)
-            return json.loads(rst)
-        except Exception as e:
-            logger.error('MarketReqUserLogin failed: %s', repr(e), exc_info=True)
-            if sub_client and sub_client.in_pubsub and channel_name1:
-                await sub_client.unsubscribe(channel_name1, channel_name2)
-                sub_client.close()
-            return None
 
     async def SubscribeMarketData(self, inst_ids: list):
         sub_client = None
@@ -140,69 +103,9 @@ class CTPApi:
                 sub_client.close()
             return None
 
-    async def TradeReqUserLogin(self, broker_id: str, user_id: str, password: str):
-        sub_client = None
-        channel_name1, channel_name2 = None, None
+    @param_function(channel='MSG:CTP:RSP:MARKET:OnRtnDepthMarketData:*')
+    async def OnRtnDepthMarketData(self, _, tick: dict):
         try:
-            sub_client = await aioredis.create_redis(
-                (config.get('REDIS', 'host', fallback='localhost'),
-                 config.getint('REDIS', 'port', fallback=6379)),
-                db=config.getint('REDIS', 'db', fallback=1))
-            request_id = self.next_id()
-            channel_name1 = self.trade_response_format.format('OnRspUserLogin', '*')
-            channel_name2 = self.trade_response_format.format('OnRspError', request_id)
-            ch1, ch2 = await sub_client.psubscribe(channel_name1, channel_name2)
-            cb = self.io_loop.create_future()
-            tasks = [
-                asyncio.ensure_future(msg_reader(ch1, cb), loop=self.io_loop),
-                asyncio.ensure_future(msg_reader(ch2, cb), loop=self.io_loop),
-            ]
-            param_dict = json.dumps({
-                'BrokerID': broker_id,
-                'UserID': user_id,
-                'Password': password,
-                'RequestID': request_id,
-            })
-            self.redis_client.publish(self.request_format.format('TradeReqUserLogin'), param_dict)
-            rst = await asyncio.wait_for(cb, HANDLER_TIME_OUT, loop=self.io_loop)
-            await sub_client.punsubscribe(channel_name1, channel_name2)
-            sub_client.close()
-            await asyncio.wait(tasks, loop=self.io_loop)
-            return json.loads(rst)
-        except Exception as e:
-            logger.error('TradeReqUserLogin failed: %s', repr(e), exc_info=True)
-            if sub_client and sub_client.in_pubsub and channel_name1:
-                await sub_client.unsubscribe(channel_name1, channel_name2)
-                sub_client.close()
-            return None
-
-    async def register_market_callback(self, func):
-        sub_client = None
-        channel_name = None
-        try:
-            sub_client = await aioredis.create_redis(
-                (config.get('REDIS', 'host', fallback='localhost'),
-                 config.getint('REDIS', 'port', fallback=6379)),
-                db=config.getint('REDIS', 'db', fallback=1))
-            request_id = self.next_id()
-            channel_name = self.trade_response_format.format('OnRtnDepthMarketData', '*')
-            ch = await sub_client.psubscribe(channel_name)
-            asyncio.ensure_future(msg_reader(ch), loop=self.io_loop)
-            param_dict = json.dumps({
-                'BrokerID': broker_id,
-                'UserID': user_id,
-                'Password': password,
-                'RequestID': request_id,
-            })
-            self.redis_client.publish(self.request_format.format('TradeReqUserLogin'), param_dict)
-            rst = await asyncio.wait_for(cb, HANDLER_TIME_OUT, loop=self.io_loop)
-            await sub_client.punsubscribe(channel_name, channel_name2)
-            sub_client.close()
-            await asyncio.wait(tasks, loop=self.io_loop)
-            return json.loads(rst)
-        except Exception as e:
-            logger.error('TradeReqUserLogin failed: %s', repr(e), exc_info=True)
-            if sub_client and sub_client.in_pubsub and channel_name:
-                await sub_client.unsubscribe(channel_name, channel_name2)
-                sub_client.close()
-            return None
+            logger.info('tick: %s', tick)
+        except Exception as ee:
+            logger.error('fresh_formula failed: %s', repr(ee), exc_info=True)
