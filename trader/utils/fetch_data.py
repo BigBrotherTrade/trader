847674c1fbe04f2e6f586c474bad4f84b5a706bd
timercrack <timercrack@gmail.com>
2016-09-26 20:36:37 +0800

    完善日线信号计算

--- trader/utils/fetch_data.py
+++ trader/utils/fetch_data.py
@@ -43,8 +43,8 @@ async def fetch_bar():
 
 
 async def fetch_bar2():
-    day = datetime.datetime.strptime('20100101', '%Y%m%d').replace(tzinfo=pytz.FixedOffset(480))
-    end = datetime.datetime.strptime('20160118', '%Y%m%d').replace(tzinfo=pytz.FixedOffset(480))
+    day = datetime.datetime.strptime('20160114', '%Y%m%d').replace(tzinfo=pytz.FixedOffset(480))
+    end = datetime.datetime.strptime('20160914', '%Y%m%d').replace(tzinfo=pytz.FixedOffset(480))
     while day <= end:
         day, trading = await is_trading_day(day)
         if trading:
@@ -65,4 +65,4 @@ async def fetch_bar2():
 # fetch_from_quandl_all()
 # clean_dailybar()
 # load_kt_data()
-calc_his_all(datetime.datetime.strptime('2016-09-22', '%Y-%m-%d').replace(tzinfo=pytz.FixedOffset(480)))
+# calc_his_all(datetime.datetime.today().replace(tzinfo=pytz.FixedOffset(480)))
